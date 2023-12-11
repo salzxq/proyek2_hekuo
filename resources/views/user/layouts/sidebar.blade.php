0360ee6b4fde35fb98cd705048589612aef4347d
Salam <ogog939@gmail.com>
2023-12-11 15:04:54 +0700

    tes

---
+++ resources/views/user/layouts/sidebar.blade.php
@@ -0,0 +1,68 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a style="height: 120px" class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('user')}}">
+      <div class="sidebar-brand-icon ">
+          @if(Auth()->user()->photo)
+            <img class="img-profile rounded-circle" src="{{Auth()->user()->photo}}">
+          @else
+            <img class="img-profile rounded-circle" src="{{asset('backend/img/avatar.png')}}">
+          @endif
+      </div>
+      
+      <div class="sidebar-brand-text mx-3">User</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('user')}}">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Shop
+        </div>
+    <!--Orders -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('user.order.index')}}">
+            <i class="fas fa-hammer fa-chart-area"></i>
+            <span>Orders</span>
+        </a>
+    </li>
+
+    <!-- Reviews -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('user.productreview.index')}}">
+            <i class="fas fa-comments"></i>
+            <span>Reviews</span></a>
+    </li>
+    
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Posts
+    </div>
+    <!-- Comments -->
+    <li class="nav-item">
+      <a class="nav-link" href="{{route('user.post-comment.index')}}">
+          <i class="fas fa-comments fa-chart-area"></i>
+          <span>Comments</span>
+      </a>
+    </li>
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
