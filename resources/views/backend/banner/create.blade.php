3d485ec3d2ed10743481f67476f5a359c39f5917
Salam <ogog939@gmail.com>
2024-01-01 22:05:23 +0700

    ubah dikit

--- resources/views/backend/banner/create.blade.php
+++ resources/views/backend/banner/create.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.master')
 
-@section('title','E-SHOP || Banner Create')
+@section('title','  || Banner Create')
 
 @section('main-content')
 
--- resources/views/backend/notification/index.blade.php
+++ resources/views/backend/notification/index.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.layouts.master')
-@section('title','E-SHOP || All Notifications')
+@section('title','  || All Notifications')
 @section('main-content')
 <div class="card">
     <div class="row">
